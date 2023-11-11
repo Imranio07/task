7276bfe01550b66082b5e3e983142fbad12ca7b4
Imranio07 <kurutkg09@gmail.com>
2023-11-11 17:40:56 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './index.css'
+function App() {
+  return (
+    <main>
+        <div className="Pokemons">
+            <div className='bulbasaur'>
+                <p>bulbasaur</p>
+            </div>
+            <div className='ivysaur'>
+                <p>ivysaur</p>
+            </div>
+            <div className='venusaur'>
+                <p>venusaur</p>
+            </div>
+            <div className='metapod'>
+                <p>metapod</p>
+            </div>
+            <div className='butterfree'>
+                <p>butterfree</p>
+            </div>
+            <div className='weedle'>
+                <p>weedle</p>
+            </div>
+        </div>
+    </main>
+  );
+}
+export default App;
